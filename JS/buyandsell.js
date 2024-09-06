4ddf032ebc579c28822dec40914e098266f58470
Ishaan Kulkarni <ishaankulkarni11@gmail.com>
2024-09-06 14:48:01 +0530

    made responsive

---
+++ JS/buyandsell.js
@@ -0,0 +1,7 @@
+document.getElementById("buyButton").addEventListener("click", function() {
+    window.location.href = "/api/v1/buyandsell/buy";
+});
+
+document.getElementById("sellButton").addEventListener("click", function() {
+    window.location.href = "/api/v1/buyandsell/sell";
+});
